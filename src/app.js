f901dff5b3cbe3de94223ba6d58b520a07ecd064
hanhit1 <ngoconghanh2k4@gmail.com>
2025-05-08 00:09:18 +0700

    feature/login and register

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const { User } = require('./models')
-app.get('/',async(req, res) => {
-  const user = await User.findAll()
-  res.send({
-    'User': user
-  }
-  );
-});
+const userRoutes = require('./routes/userRoutes');
+
+// Middleware
+app.use(express.json());
+
+app.use('/', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+
+// Route đăng nhập
+router.post('/login', userController.login);
+
+// Route đăng ký
+router.post('/register', userController.register);
+
+
+module.exports = router;
