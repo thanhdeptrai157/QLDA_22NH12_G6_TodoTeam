af4c0b6331fb764aa04a09cf42afe2cb7c0888db
hanhit1 <ngoconghanh2k4@gmail.com>
2025-05-08 10:39:33 +0700

    feat: Add new column to post table

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -11,7 +11,7 @@ Post.belongsTo(User, { foreignKey: 'userId' });
 User.hasMany(Comment, { foreignKey: 'user_id' });
 Comment.belongsTo(User, { foreignKey: 'user_id' });
 
-Post.hasMany(Comment, { foreignKey: 'post_id' });
+Post.hasMany(Comment, {as: 'postComments', foreignKey: 'post_id' });
 Comment.belongsTo(Post, { foreignKey: 'post_id' });
 
 Post.belongsTo(Category, { foreignKey: 'category_id' });
