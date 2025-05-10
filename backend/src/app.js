d27e3645a4c4d9b3a9ef64ec4d53bf6d5337fcf0
hanhit1 <ngoconghanh2k4@gmail.com>
2025-05-10 22:31:15 +0700

    fix: login

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 
 const authRoutes = require('./routes/auth.routes');
 const postRoutes = require('./routes/post.routes');
+const userRoutes = require('./routes/user.routes')
 
-app.use(express.json());
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
+app.use('',userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt'); 
 const jwt = require('jsonwebtoken');
-const userService = require('../services/userService');
+const userService = require('../services/user.service');
 // Đăng nhập
 const login = async (req, res) => {
   const { email, password } = req.body;
