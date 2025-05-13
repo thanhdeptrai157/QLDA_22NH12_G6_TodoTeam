6da440b5f1cac5a4d5f42161422e5b373c32900c
Thanh <thanhcap3@gmail.com>
2025-05-13 08:52:52 +0700

    feat: add api goong, auto complete search place

--- frontend/configs/env.ts
+++ frontend/configs/env.ts
@@ -1,1 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const GOONG_API_URL = process.env.NEXT_PUBLIC_GOONG_API_URL;
+export const GOONG_API_KEY = process.env.NEXT_PUBLIC_GOONG_API_KEY;
